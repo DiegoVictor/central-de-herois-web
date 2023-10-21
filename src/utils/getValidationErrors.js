5e741d8923861eb54d399fbccc2101f7e54eba51
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-10-21 08:27:12 -0300

    Test getValidationErrors

---
+++ src/utils/getValidationErrors.js
@@ -0,0 +1,10 @@
+export const getValidationErrors = err => {
+  const validationErrors = {};
+
+  err.inner.forEach(error => {
+    if (error.path) {
+      validationErrors[error.path] = error.message;
+    }
+  });
+  return validationErrors;
+};
