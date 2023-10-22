b54b814356be07e0136717ef1ad56f92ac9d2ca5
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-10-22 14:50:25 -0300

    Update Input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,20 +2,35 @@ import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useField } from '@unform/core';
 
-export default function Input({ name, ...rest }) {
-  const inputRef = useRef(null);
+import { Error } from './styles';
 
-  const { fieldName, defaultValue = '', registerField } = useField(name);
+export default function Input({ name, ...rest }) {
+  const inputRef = useRef();
+
+  const { fieldName, defaultValue, registerField, error } = useField(name);
 
   useEffect(() => {
     registerField({
       name: fieldName,
-      ref: inputRef.current,
-      path: 'value',
+      ref: inputRef,
+      getValue: (ref) => {
+        return ref.current.value;
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value;
+      },
+      clearValue: (ref) => {
+        ref.current.value = '';
+      },
     });
   }, [fieldName, registerField]);
 
-  return <input ref={inputRef} defaultValue={defaultValue} {...rest} />;
+  return (
+    <>
+      <input ref={inputRef} defaultValue={defaultValue} {...rest} />
+      {error && <Error>{error}</Error>}
+    </>
+  );
 }
 
 Input.propTypes = {
---
+++ src/components/Input/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Error = styled.span`
+  color: #c72828;
+  display: block;
+  margin-top: 5px;
+  margin-bottom: 20px;
+`;
