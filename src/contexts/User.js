38f51c3e0e1592c961a4249b5b653d0c131cde62
DiegoVictor <diegovictorgonzaga@gmail.com>
2023-10-22 14:51:54 -0300

    Simplify context data loading

--- src/contexts/User.js
+++ src/contexts/User.js
@@ -1,8 +1,5 @@
 import { createContext } from 'react';
 
-let user = {};
-if (typeof localStorage.iheroes_user !== 'undefined') {
-  user = JSON.parse(localStorage.getItem('iheroes_user'));
-}
+const user = JSON.parse(localStorage.getItem('iheroes_user') || '{}');
 
 export default createContext(user);
