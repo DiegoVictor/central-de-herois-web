57dfacaa33142f31150a294736301a500f069e23
Diego Victor <diegovictorgonzaga@gmail.com>
2023-10-31 10:37:20 -0300

    Create auth token setter

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+export const setAuthHeader = (value) => {
+  api.defaults.headers.common.Authorization = value;
+};
+
 export default api;
