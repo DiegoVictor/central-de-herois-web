69eda59d950350c32ae40203623ea677b4007f21
Diego Victor <diegovictorgonzaga@gmail.com>
2023-11-02 10:07:57 -0300

    Fix linting

--- src/pages/Heroes/index.js
+++ src/pages/Heroes/index.js
@@ -1,10 +1,4 @@
-import React, {
-  useEffect,
-  useState,
-  useContext,
-  useMemo,
-  useCallback,
-} from 'react';
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import {
   Form as Frm,
   Table,
@@ -33,7 +27,7 @@ export function Heroes() {
       try {
         await api.delete(`/heroes/${id}`);
 
-        setHeroes(heroes.filter((hero) => hero._id !== id));
+        setHeroes(heroes.filter(({ _id }) => _id !== id));
       } catch (err) {
         alert('Não foi possivel remover o heroi, tente novamente!');
       }
@@ -42,11 +36,11 @@ export function Heroes() {
   );
 
   const handleHeroForm = useCallback(
-    ({ name, rank, status, latitude, longitude }, notify) => {
+    ({ _id, name, rank, status, latitude, longitude }) => {
       (async () => {
-        if (hero._id) {
+        if (_id) {
           try {
-            const response = await api.put(`/heroes/${hero._id}`, {
+            const response = await api.put(`/heroes/${_id}`, {
               name,
               rank,
               status,
@@ -56,7 +50,7 @@ export function Heroes() {
 
             setHeroes(
               heroes.map((h) => {
-                if (h._id === hero._id) {
+                if (h._id === _id) {
                   return response.data;
                 }
                 return h;
@@ -88,16 +82,13 @@ export function Heroes() {
 
   useEffect(() => {
     (async () => {
-      const { data } = await api.get('heroes', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const { data } = await api.get('heroes');
+
       setHeroes(
-        data.map((hero) => ({
-          ...hero,
-          latitude: hero.location.coordinates[1],
-          longitude: hero.location.coordinates[0],
+        data.map(({ coordinates: [longitude, latitude], ...props }) => ({
+          ...props,
+          latitude,
+          longitude,
         }))
       );
     })();
@@ -112,150 +103,141 @@ export function Heroes() {
               Novo
             </Button>
           </div>
-                <Table hover striped size="sm">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>Rank</th>
-                      <th>Localização</th>
-                      <th>Status</th>
-                      <th />
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {heroes.map((hero) => (
-                      <tr key={hero._id} data-testid={`hero_${hero._id}`}>
-                        <td>{hero.name}</td>
-                        <td data-testid={`hero_rank_${hero._id}`}>
-                          {hero.rank}
-                        </td>
-                        <td>
-                          <Link
-                            to={`${
-                              googleMapUrl + hero.location.coordinates[1]
-                            },${hero.location.coordinates[0]}`}
-                            target="_blank"
-                          >
-                            {hero.location.coordinates
-                              .slice()
-                              .reverse()
-                              .join(', ')}
-                          </Link>
-                        </td>
-                        <td data-testid={`hero_status_${hero._id}`}>
-                          {getLabel(hero.status)}
-                        </td>
-                        <td className="text-right">
-                          <ButtonGroup>
-                            <Button
-                              data-testid={`hero_edit_${hero._id}`}
-                              size="sm"
-                              onClick={() => {
+          <Table hover striped size="sm">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Rank</th>
+                <th>Localização</th>
+                <th>Status</th>
+                <th aria-label="Ações" />
+              </tr>
+            </thead>
+            <tbody>
+              {heroes.map((hero) => (
+                <tr key={hero._id} data-testid={`hero_${hero._id}`}>
+                  <td>{hero.name}</td>
+                  <td data-testid={`hero_rank_${hero._id}`}>{hero.rank}</td>
+                  <td>
+                    <Link
+                      to={`${googleMapUrl + hero.location.coordinates[1]},${
+                        hero.location.coordinates[0]
+                      }`}
+                      target="_blank"
+                    >
+                      {hero.location.coordinates.slice().reverse().join(', ')}
+                    </Link>
+                  </td>
+                  <td data-testid={`hero_status_${hero._id}`}>
+                    {getLabel(hero.status)}
+                  </td>
+                  <td className="text-right">
+                    <ButtonGroup>
+                      <Button
+                        data-testid={`hero_edit_${hero._id}`}
+                        size="sm"
+                        onClick={() => {
                           setFormData(hero);
-                              }}
-                            >
-                              Editar
-                            </Button>
-                            <Button
-                              data-testid={`hero_remove_${hero._id}`}
-                              disabled={hero.status === 'fighting'}
-                              size="sm"
-                              onClick={() => {
+                        }}
+                      >
+                        Editar
+                      </Button>
+                      <Button
+                        data-testid={`hero_remove_${hero._id}`}
+                        disabled={hero.status === 'fighting'}
+                        size="sm"
+                        onClick={() => {
                           handleRemoveHero(hero._id);
-                              }}
-                            >
-                              Remover
-                            </Button>
-                          </ButtonGroup>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </Table>
+                        }}
+                      >
+                        Remover
+                      </Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
 
           <Modal
             title="Heroi"
             show={!!formData}
             onHide={() => setFormData(null)}
           >
-                  <Form
+            <Form
               initialData={formData}
-                    onSubmit={(data) => {
+              onSubmit={(data) => {
                 handleHeroForm(data);
-                    }}
-                  >
-                    <Frm.Group>
-                      <Frm.Label>Nome</Frm.Label>
-                      <Input
-                        className="form-control"
-                        name="name"
-                        data-testid="name"
-                      />
-                    </Frm.Group>
-                    <Frm.Group>
-                      <Frm.Label>Rank</Frm.Label>
-                      <Select
-                        className="form-control"
-                        name="rank"
-                        data-testid="rank"
-                      >
-                        {['S', 'A', 'B', 'C'].map((rank) => (
-                          <option key={rank} value={rank}>
-                            {rank}
-                          </option>
-                        ))}
-                      </Select>
-                    </Frm.Group>
-                    <Row>
-                      <Col>
-                        <Frm.Group>
-                          <Frm.Label>Latitude</Frm.Label>
-                          <Input
-                            className="form-control"
-                            name="latitude"
-                            data-testid="latitude"
-                          />
-                        </Frm.Group>
-                      </Col>
-                      <Col>
-                        <Frm.Group>
-                          <Frm.Label>Longitude</Frm.Label>
-                          <Input
-                            className="form-control"
-                            name="longitude"
-                            data-testid="longitude"
-                          />
-                        </Frm.Group>
-                      </Col>
-                    </Row>
+              }}
+            >
+              <Frm.Group>
+                <Frm.Label>Nome</Frm.Label>
+                <Input
+                  className="form-control"
+                  name="name"
+                  data-testid="name"
+                />
+              </Frm.Group>
+              <Frm.Group>
+                <Frm.Label>Rank</Frm.Label>
+                <Select className="form-control" name="rank" data-testid="rank">
+                  {['S', 'A', 'B', 'C'].map((rank) => (
+                    <option key={rank} value={rank}>
+                      {rank}
+                    </option>
+                  ))}
+                </Select>
+              </Frm.Group>
+              <Row>
+                <Col>
+                  <Frm.Group>
+                    <Frm.Label>Latitude</Frm.Label>
+                    <Input
+                      className="form-control"
+                      name="latitude"
+                      data-testid="latitude"
+                    />
+                  </Frm.Group>
+                </Col>
+                <Col>
+                  <Frm.Group>
+                    <Frm.Label>Longitude</Frm.Label>
+                    <Input
+                      className="form-control"
+                      name="longitude"
+                      data-testid="longitude"
+                    />
+                  </Frm.Group>
+                </Col>
+              </Row>
               {formData && formData.status !== 'fighting' && (
-                      <Frm.Group>
-                        <Frm.Label>Status</Frm.Label>
-                        <Select
-                          className="form-control"
-                          name="status"
-                          data-testid="status"
-                        >
-                          <option value="resting">Descasando</option>
-                          <option value="out_of_combat">Fora de Combate</option>
-                          <option value="patrolling">Patrulhando</option>
-                        </Select>
-                      </Frm.Group>
-                    )}
-                    <ButtonGroup>
-                      <Button
-                        data-testid="cancel"
-                        variant="secondary"
+                <Frm.Group>
+                  <Frm.Label>Status</Frm.Label>
+                  <Select
+                    className="form-control"
+                    name="status"
+                    data-testid="status"
+                  >
+                    <option value="resting">Descasando</option>
+                    <option value="out_of_combat">Fora de Combate</option>
+                    <option value="patrolling">Patrulhando</option>
+                  </Select>
+                </Frm.Group>
+              )}
+              <ButtonGroup>
+                <Button
+                  data-testid="cancel"
+                  variant="secondary"
                   onClick={() => setFormData(null)}
-                      >
-                        Cancelar
-                      </Button>
-                      <Button data-testid="submit" type="submit">
-                        Enviar
-                      </Button>
-                    </ButtonGroup>
-                  </Form>
-                </Modal>
+                >
+                  Cancelar
+                </Button>
+                <Button data-testid="submit" type="submit">
+                  Enviar
+                </Button>
+              </ButtonGroup>
+            </Form>
+          </Modal>
         </Col>
       </Row>
     </Container>
