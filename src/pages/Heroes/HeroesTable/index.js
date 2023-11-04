5e738c9ddb6162a9a2f948209624a26f9f9f44e8
Diego Victor <diegovictorgonzaga@gmail.com>
2023-11-04 11:23:51 -0300

    Move heroes table to its own component

---
+++ src/pages/Heroes/HeroesTable/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Table, Button, ButtonGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { getLabel } from '~/helpers/HeroStatuses';
+import { googleMapUrl } from '~/utils/constants';
+
+export function HeroesTable({ heroes, handleRemoveHero, setFormData }) {
+  return (
+    <Table hover striped size="sm">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Rank</th>
+          <th>Localização</th>
+          <th>Status</th>
+          <th aria-label="Ações" />
+        </tr>
+      </thead>
+      <tbody>
+        {heroes.map((hero) => (
+          <tr key={hero._id} data-testid={`hero_${hero._id}`}>
+            <td>{hero.name}</td>
+            <td data-testid={`hero_rank_${hero._id}`}>{hero.rank}</td>
+            <td>
+              <Link
+                to={`${googleMapUrl}${hero.latitude},${hero.longitude}`}
+                target="_blank"
+              >
+                {hero.latitude},{hero.longitude}
+              </Link>
+            </td>
+            <td data-testid={`hero_status_${hero._id}`}>
+              {getLabel(hero.status)}
+            </td>
+            <td className="text-right">
+              <ButtonGroup>
+                <Button
+                  data-testid={`hero_edit_${hero._id}`}
+                  size="sm"
+                  onClick={() => {
+                    setFormData(hero);
+                  }}
+                >
+                  Editar
+                </Button>
+                <Button
+                  data-testid={`hero_remove_${hero._id}`}
+                  disabled={hero.status === 'fighting'}
+                  size="sm"
+                  onClick={() => {
+                    handleRemoveHero(hero._id);
+                  }}
+                >
+                  Remover
+                </Button>
+              </ButtonGroup>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+HeroesTable.propTypes = {
+  heroes: PropTypes.arrayOf({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    rank: PropTypes.string.isRequired,
+    latitude: PropTypes.string.isRequired,
+    longitude: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }).isRequired,
+  handleRemoveHero: PropTypes.func.isRequired,
+  setFormData: PropTypes.func.isRequired,
+};
--- src/pages/Heroes/index.js
+++ src/pages/Heroes/index.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { Table, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Row, Col, Button } from 'react-bootstrap';
 
-import { getLabel } from '~/helpers/HeroStatuses';
 import api from '~/services/api';
-import { googleMapUrl } from '~/utils/constants';
 import { Container } from './styles';
 import { FormModal } from './FormModal';
+import { HeroesTable } from './HeroesTable';
 
 export function Heroes() {
   const [heroes, setHeroes] = useState([]);
@@ -87,69 +85,17 @@ export function Heroes() {
               Novo
             </Button>
           </div>
-          <Table hover striped size="sm">
-            <thead>
-              <tr>
-                <th>Nome</th>
-                <th>Rank</th>
-                <th>Localização</th>
-                <th>Status</th>
-                <th aria-label="Ações" />
-              </tr>
-            </thead>
-            <tbody>
-              {heroes.map((hero) => (
-                <tr key={hero._id} data-testid={`hero_${hero._id}`}>
-                  <td>{hero.name}</td>
-                  <td data-testid={`hero_rank_${hero._id}`}>{hero.rank}</td>
-                  <td>
-                    <Link
-                      to={`${googleMapUrl + hero.location.coordinates[1]},${
-                        hero.location.coordinates[0]
-                      }`}
-                      target="_blank"
-                    >
-                      {hero.location.coordinates.slice().reverse().join(', ')}
-                    </Link>
-                  </td>
-                  <td data-testid={`hero_status_${hero._id}`}>
-                    {getLabel(hero.status)}
-                  </td>
-                  <td className="text-right">
-                    <ButtonGroup>
-                      <Button
-                        data-testid={`hero_edit_${hero._id}`}
-                        size="sm"
-                        onClick={() => {
-                          setFormData(hero);
-                        }}
-                      >
-                        Editar
-                      </Button>
-                      <Button
-                        data-testid={`hero_remove_${hero._id}`}
-                        disabled={hero.status === 'fighting'}
-                        size="sm"
-                        onClick={() => {
-                          handleRemoveHero(hero._id);
-                        }}
-                      >
-                        Remover
-                      </Button>
-                    </ButtonGroup>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-
-          {formData && (
-            <FormModal
-              formData={formData}
-              handleHeroForm={handleHeroForm}
-              onHide={() => setFormData(null)}
-            />
-          )}
+          <HeroesTable
+            heroes={heroes}
+            handleRemoveHero={handleRemoveHero}
+            setFormData={setFormData}
+          />
+          (
+          <FormModal
+            formData={formData}
+            handleHeroForm={handleHeroForm}
+            onHide={() => setFormData(null)}
+          />
         </Col>
       </Row>
     </Container>
