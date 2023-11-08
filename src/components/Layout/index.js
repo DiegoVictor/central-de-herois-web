ced3e8d492896a3a1a80df7d6788cec3e5cdacd2
Diego Victor <diegovictorgonzaga@gmail.com>
2023-11-08 10:04:54 -0300

    Move layout styles to theme file

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 
 import UserContext from '~/contexts/User';
 import { Menu } from '~/components/Menu';
-import Theme, { Container } from './styles';
+import Theme, { Container } from '~/styles/theme';
 
 export function Layout() {
   const { token } = useContext(UserContext);
