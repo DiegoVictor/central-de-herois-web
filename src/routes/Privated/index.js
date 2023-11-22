aa354db64e6ba8bf1e9c02df39cb49d4a3bff0ef
Diego Victor <diegovictorgonzaga@gmail.com>
2023-11-22 09:57:52 -0300

    Redirect on conditions

--- src/routes/Privated/index.js
+++ src/routes/Privated/index.js
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react';
-import { redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { UserContext } from '~/contexts/User';
 
 export function Privated({ element: Component }) {
-  const { token } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-  if (!token) {
-    return redirect('/');
+  if (!user?.token) {
+    return <Navigate to="/" />;
   }
 
   return <Component />;
