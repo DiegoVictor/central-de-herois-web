7e4013d4633c435c5d6e27096248300b64df0a3b
Diego Victor <diegovictorgonzaga@gmail.com>
2023-11-25 10:08:09 -0300

    Create Title component

---
+++ src/components/Title/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  margin-bottom: 40px;
+
+  span {
+    background-color: #0b4cdb;
+    border-radius: 4px;
+    color: white;
+    display: inline-block;
+    margin-right: 2px;
+    padding: 5px 15px;
+  }
+`;
