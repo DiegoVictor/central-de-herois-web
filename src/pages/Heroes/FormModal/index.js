168fa64331f43eedd2df9d4458a4efc55fc1dfcf
Diego Victor <diegovictorgonzaga@gmail.com>
2023-12-01 08:38:56 -0300

    Udate hero form

--- src/pages/Heroes/FormModal/index.js
+++ src/pages/Heroes/FormModal/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Form as Unform } from '@unform/web';
-import { Form, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
+import { Form, Row, Col, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
 import PropTypes from 'prop-types';
 
 import { Input } from '~/components/Input';
 import { Select } from '~/components/Select';
-import { Modal } from '~/components/Modal';
 
 export function FormModal({ formData = null, handleHeroForm, onHide }) {
   return (
@@ -17,63 +17,74 @@ export function FormModal({ formData = null, handleHeroForm, onHide }) {
           handleHeroForm({ _id, ...data });
         }}
       >
-        <Form.Group>
-          <Form.Label>Nome</Form.Label>
-          <Input className="form-control" name="name" data-testid="name" />
-        </Form.Group>
+        <Modal.Header closeButton>
+          <Modal.Title>Heroi</Modal.Title>
+        </Modal.Header>
 
-        <Form.Group>
-          <Form.Label>Rank</Form.Label>
-          <Select className="form-control" name="rank" data-testid="rank">
-            {['S', 'A', 'B', 'C'].map((rank) => (
-              <option key={rank} value={rank}>
-                {rank}
-              </option>
-            ))}
-          </Select>
-        </Form.Group>
+        <Modal.Body>
+          <Form.Group className="mb-3">
+            <Form.Label>Nome</Form.Label>
+            <Input className="form-control" name="name" data-testid="name" />
+          </Form.Group>
 
-        <Row>
-          <Col>
-            <Form.Group>
-              <Form.Label>Latitude</Form.Label>
-              <Input
-                className="form-control"
-                name="latitude"
-                data-testid="latitude"
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group>
-              <Form.Label>Longitude</Form.Label>
-              <Input
-                className="form-control"
-                name="longitude"
-                data-testid="longitude"
-              />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        {formData?.status !== 'fighting' && (
-          <Form.Group>
-            <Form.Label>Status</Form.Label>
-            <Select className="form-control" name="status" data-testid="status">
-              <option value="resting">Descasando</option>
-              <option value="out_of_combat">Fora de Combate</option>
-              <option value="patrolling">Patrulhando</option>
+          <Form.Group className="mb-3">
+            <Form.Label>Rank</Form.Label>
+            <Select className="form-control" name="rank" data-testid="rank">
+              {['S', 'A', 'B', 'C'].map((rank) => (
+                <option key={rank} value={rank}>
+                  {rank}
+                </option>
+              ))}
             </Select>
           </Form.Group>
-        )}
-        <ButtonGroup>
+
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Label>Latitude</Form.Label>
+                <Input
+                  className="form-control"
+                  name="latitude"
+                  data-testid="latitude"
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Label>Longitude</Form.Label>
+                <Input
+                  className="form-control"
+                  name="longitude"
+                  data-testid="longitude"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+
+          {formData?.status !== 'fighting' && (
+            <Form.Group className="mt-3">
+              <Form.Label>Status</Form.Label>
+              <Select
+                className="form-control"
+                name="status"
+                data-testid="status"
+              >
+                <option value="resting">Descasando</option>
+                <option value="out_of_combat">Fora de Combate</option>
+                <option value="patrolling">Patrulhando</option>
+              </Select>
+            </Form.Group>
+          )}
+        </Modal.Body>
+
+        <Modal.Footer>
           <Button data-testid="cancel" variant="secondary" onClick={onHide}>
             Cancelar
           </Button>
-          <Button data-testid="submit" type="submit">
+          <Button data-testid="submit" variant="success" type="submit">
             Enviar
           </Button>
-        </ButtonGroup>
+        </Modal.Footer>
       </Unform>
     </Modal>
   );
